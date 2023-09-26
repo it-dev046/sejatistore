dade429ae7cc563cded236d9bab5204933219fdf
Admin <it.webdev46@gmail.com>
2023-09-26 18:54:59 +0800

    coba

---
+++ app/Models/StatusModel.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class StatusModel extends Model
+{
+    protected $table            = 'status';
+    protected $primaryKey       = 'id_status';
+    protected $returnType       = 'object';
+    protected $allowedFields    = ['status', 'potongan'];
+}
